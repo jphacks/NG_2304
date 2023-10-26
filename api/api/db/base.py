09c6f26773c8608722b61b313b7f37d23aff9944
HEKUCHAN <heitorhirose@gmail.com>
2023-10-26 23:58:37 +0900

    #14 Add craeted_at, updated_at

--- api/api/db/base.py
+++ api/api/db/base.py
@@ -1,4 +1,7 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from api.db.meta import meta
 
@@ -7,3 +10,15 @@ class Base(DeclarativeBase):
     """Base for all models."""
 
     metadata = meta
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=func.now(),
+        onupdate=func.now(),
+        nullable=True,
+    )
