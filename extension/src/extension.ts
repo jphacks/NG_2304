907acf96421b52641cf73abcaba4633a873c4ac6
Milix-M <tujikou12dh@gmail.com>
2023-10-27 22:59:21 +0900

    githublogin コマンドを定義

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -13,10 +13,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from extension!');
+	let disposable = vscode.commands.registerCommand('extension.githubLogin', () => {
+
+		vscode.window.showInformationMessage('Github Login Command');
 	});
 
 	context.subscriptions.push(disposable);
